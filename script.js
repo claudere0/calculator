49597dc31b946dd68e541a98c0ed494812e5022a
claudere <129028339+claudere0@users.noreply.github.com>
2026-04-29 16:26:50 +0500

    main logic and functions maybe not so right but it works

--- script.js
+++ script.js
@@ -1,20 +1,28 @@
+let firstValue = '';
+let secondValue = '';
+let operator = '';
+let waitingForSecondValue = true;
+let currentInput = '';
+let previousInput = '';
+let shouldResetScreen = false
+
 function add(a,b) {
-    return a+b
+    return Number(a) + Number(b)
 }
 
 function subtract(a,b) {
-    return a-b
+    return Number(a) - Number(b)
 }
 
 function multiply(a,b) {
-    return a*b
+    return Number(a) * Number(b)
 }
 
 function divide(a,b) {
-    return a/b
+    return Number(a) / Number(b)
 }
 
-function operate(a, oper, b) {
+function calculate(a, oper, b) {
     if (oper === "+") {
         return add(a,b)
     }
@@ -29,8 +37,58 @@ function operate(a, oper, b) {
     }
 }
 
-console.log(add(10,90))
-console.log(subtract(10,90))
-console.log(multiply(10,90))
-console.log(divide(10,90))
-console.log(operate(400, '*', 144))
+let display = document.getElementById("display")
+
+function updateDisplay(input) {
+    display.textContent = input;
+}
+
+const buttons = document.querySelectorAll('.button');
+
+buttons.forEach(button => {
+
+    button.addEventListener('click', (e) => {
+        const value = e.target.innerText;
+
+        if (value === 'clear') {
+            firstValue = '';
+            secondValue = '';
+            operator = '';
+            currentInput = '';
+            previousInput = '';
+            waitingForSecondValue = true;
+            shouldResetScreen = false;
+            updateDisplay('');
+        } else if (value === '=') {
+            secondValue = currentInput
+            currentInput = ''
+            previousInput = ''
+            
+            result = calculate(firstValue, operator, secondValue)
+            updateDisplay(result)
+            operator = ''
+            firstValue = result
+            
+        } else if (value === '+' || value === '-' || value === '*' || value === '/') {
+            if (currentInput.length > 0) {
+                firstValue = currentInput
+                operator = value
+                updateDisplay(value)
+                currentInput = ''
+                previousInput = ''
+            } else {
+                operator = value
+                updateDisplay(value)
+                currentInput = ''
+                previousInput = ''
+            }
+        } else {
+            currentInput = previousInput + value
+            previousInput = currentInput
+            updateDisplay(currentInput)
+        }
+        console.log(value)
+    });
+});
+
+
